bb95c8fb9413704ceb1b8e9f745ad2d7b90979da
otaviohud <otavio.favoni.tato@gmail.com>
2022-10-26 17:45:08 +0000

    Estrutura

---
+++ app/src/main/java/otaviofavoni/models/Genero.java
@@ -0,0 +1,32 @@
+package otaviofavoni.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "generos")
+public class Genero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nome;
+
+    public int getId() {
+        return this.id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
+
---
+++ app/src/main/java/otaviofavoni/repositories/GeneroRepository.java
@@ -0,0 +1,9 @@
+package otaviofavoni.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import otaviofavoni.models.Genero;
+
+public interface GeneroRepository extends CrudRepository<Genero, Integer> {
+   
+}
