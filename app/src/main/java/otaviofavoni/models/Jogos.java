019fda8d804f8077b381544dba086c5e65554ee3
otaviohud <otavio.favoni.tato@gmail.com>
2022-11-07 17:11:27 +0000

    games

---
+++ app/src/main/java/otaviofavoni/models/Jogos.java
@@ -0,0 +1,27 @@
+package otaviofavoni.models;
+
+public class Jogo {
+    private int id;
+    private String titulo;
+
+    private int idGenero;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return this.id;
+    }
+     public void setTitulo(String titulo) {
+        this.id = titulo;
+     }
+     public String getTitulo() {
+        return this.titulo;
+     }
+    public int getIdGenero(int idGenero) {
+        this.idGenero;
+    }
+    public int getIdGenero() {
+        return this.idGenero;
+    }
+}
